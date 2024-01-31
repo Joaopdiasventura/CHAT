dfa914859cedbbc3a3076d55df2440dcac9c4b4a
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-01-30 22:31:14 -0300

    fix: especificando o host

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,11 +1,11 @@
-// server.ts
 import { FastifyInstance } from 'fastify';
 import { AddressInfo } from 'net';
 
 export default async function startServer(app: FastifyInstance) {
   try {
-    const port = parseInt(process.env.PORT);
-    await app.listen({ port });
+    const port = parseInt(process.env.PORT || '3000');
+
+    await app.listen({ port, host: '0.0.0.0' });
 
     const addressInfo = app.server.address() as AddressInfo;
     const actualPort = addressInfo.port;
