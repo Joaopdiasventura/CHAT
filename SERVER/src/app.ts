f2b94c653fd34f73eddde6dae7ddab6757e9a911
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-02-01 15:34:58 -0300

    fix: ajustando o cors e a versão do JavaScript

--- SERVER/src/app.ts
+++ SERVER/src/app.ts
@@ -12,7 +12,7 @@ import token from './routes/token';
 const app = fastify();
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://chat-two-ochre-97.vercel.app/',
   methods: ['GET', 'PUT', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
